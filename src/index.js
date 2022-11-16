15923da13d796959c39e01cfebbf73081f06934d
Odhran <mahonyodhran@gmail.com>
2022-11-16 23:09:38 +0000

    added todos

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import Header from './components/Header';
+import Todos from './components/Todos'
 
 function App() {
   return (
     <ChakraProvider>
       <Header />
+      <Todos />
     </ChakraProvider>
   );
 }
